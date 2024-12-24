625a5772ce5a733b7c8433e3f4f9b297a9e80da1
HagridCode <rushikshirsagar1698@gmail.com>
2024-12-24 17:15:48 +0530

    newrepotestwork

---
+++ src/test/java/testhrmweb/LoginTrail.java
@@ -0,0 +1,55 @@
+package testhrmweb;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTrail {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.orangehrm.com/");
+
+        //Thread.sleep(3000);
+       
+        WebElement EmailHomePage = driver.findElement(By.id("Form_submitForm_EmailHomePage"));
+       EmailHomePage.sendKeys("testuser@example.com");
+       Thread.sleep(1000);
+       
+       WebElement actionrequest = driver.findElement(By.id("Form_submitForm_action_request")); // Replace with the correct ID if necessary
+       actionrequest.click();
+       Thread.sleep(1000);
+       
+       WebElement subdomain = driver.findElement(By.id("Form_getForm_subdomain")); // Replace with the correct ID if necessary
+       subdomain.sendKeys("quantamHR");
+       Thread.sleep(1000);
+       
+       WebElement Fullname = driver.findElement(By.id("Form_getForm_Name")); // Replace with the correct ID if necessary
+       Fullname.sendKeys("HR Ashwini Reddy");
+       Thread.sleep(1000);
+       
+       WebElement Contact = driver.findElement(By.id("Form_getForm_Contact")); // Replace with the correct ID if necessary
+       Contact.sendKeys("9182548528");
+       Thread.sleep(1000);
+        
+       WebElement countryDropdown = driver.findElement(By.id("Form_getForm_Country")); // Replace with the actual ID
+        
+            // Create an instance of the Select class
+            Select selectCountry = new Select(countryDropdown);
+            // Select a country by visible text
+            selectCountry.selectByVisibleText("India");
+            Thread.sleep(60000); // 60 seconds (adjust as needed)
+
+            // Locate and click the "Get Your Free Trial" button
+       WebElement freeTrialButton = driver.findElement(By.id("Form_getForm_action_submitForm")); // Replace with actual ID
+            freeTrialButton.click();
+
+       driver.manage().window().maximize();   
+    
+    }
+}
---
+++ src/test/java/testhrmweb/Testhrmhome.java
@@ -0,0 +1,18 @@
+package testhrmweb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testhrmhome {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.orangehrm.com/");
+
+        Thread.sleep(3000);
+
+    }
+}
